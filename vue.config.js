f3a43e14dd964d3489794bca2ec09bf1803d44a5
tasuku677 <78686862+tasuku677@users.noreply.github.com>
2024-07-01 13:45:51 +0200

    create a vue project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-module.exports = {
-    devServer: {
-       proxy: 'http://127.0.0.1:5500',
-    }
-  }
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
