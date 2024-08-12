3a7f6cb7b3d5b3d45443143cbc62cdab370d33b6
tasuku677 <78686862+tasuku677@users.noreply.github.com>
2024-08-12 08:27:25 +0200

    wrote README.md

--- src/scripts/getAssetInfo.js
+++ src/scripts/getAssetInfo.js
@@ -1,39 +1,36 @@
-const config = {
-  // assetUrl: "http://localhost:5501/assets/SANSK5A001x",
-  assetUrl:"http://localhost:5500/assets/car-dodge",
-  // assetUrl:"http://localhost:5500/assets/formula",
-  port: 9081,
-}
-
 async function getAssetInfo() {
   try {
-      const response = await fetch(config.assetUrl);
-      const data = await response.json();
-      const id = extractId(data['Url']);
-      let objectData = {
-        imageUrl:"http://localhost:" + config.port + '/image/' + id.replace('.', ''),
-        assetId: id.replace('.', ''),
-        versionId: data["VersionId"],
-      }
-      return JSON.stringify(objectData);
+    const baseUrl = window.location.href.match(/^[^/]+\/\/[^/]+/)[0];
+    console.log('baseUrl:', baseUrl);
+    const [assetId, query] = window.location.href.substring(baseUrl.length + 1).split('?');
+    const portNumber = query.replace('port=', '');
+    console.log('assetId:', assetId, 'query:', query, 'portNumber:', portNumber);
+    console.log(`${baseUrl}/assets/${assetId}`);
+
+    const response = await fetch(`${baseUrl}/assets/${assetId}`);
+    const data = await response.json();
+    let objectData = {
+      imageUrl: `${baseUrl.replace(/:\d+$/, '')}:${portNumber}/image/${assetId}`,
+      assetId: assetId,
+      versionId: data["VersionId"],
+    }
+    return JSON.stringify(objectData);
   } catch (error) {
-      console.error('Error fetching version ID:', error);
-      return null;
+    console.error('Error fetching version ID:', error);
+    return null;
   }
 }
 
-function extractId(url) {
-  // URLを"/"で分割し、最後の要素を取得
-  let parts = url.split('/');
-  let filename = parts[parts.length - 1];
+// function extractId(url) {
+//   let parts = url.split('/');
+//   let filename = parts[parts.length - 1];
 
-  // 拡張子があれば取り除く
-  let lastIndex = filename.lastIndexOf('.');
-  if (lastIndex !== -1) {
-    filename = filename.substring(0, lastIndex);
-  }
+//   let lastIndex = filename.lastIndexOf('.');
+//   if (lastIndex !== -1) {
+//     filename = filename.substring(0, lastIndex);
+//   }
 
-  return filename;
-}
+//   return filename;
+// }
 // getAssetInfo();
-export{getAssetInfo};
+export { getAssetInfo };
